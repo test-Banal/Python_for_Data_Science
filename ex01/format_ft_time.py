322a9e8c559f6b508831b29334518c5f6c03d521
Alban Neumann <aneumann@f4r3s14.clusters.42paris.fr>
2026-03-30 20:08:17 +0200

    ex01 Python Piscine

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,21 @@
+import time
+from datetime import datetime
+
+current_timestamp  = time.time()
+print(f"Seconds since January 1, 1970: {current_timestamp:,.4f} or {current_timestamp:.2e} in scientific notation")
+
+dttime = datetime.fromtimestamp(current_timestamp)
+date_format = dttime.strftime("%b %d %Y")
+print(date_format)
+
+
+#Ressources :
+#notation scientifique : https://ladatascience.fr/2023/02/07/utiliser-la-notation-scientifique-en-python/
+#heure depuis 01/01/1970 : https://labex.io/fr/tutorials/python-how-to-get-system-time-in-python-435418
+#date : https://docs.python.org/fr/3.7/library/datetime.html
+#%b mois abrege, %d jour sur deux chiffres, %Y anne sur 4 chiffres
+#fromtimestamp = converti nmbr en second en date et heure
+
+#from datetime import datetime
+#date = datetime.now().strftime("%b %d %Y" )
+#print(date)
