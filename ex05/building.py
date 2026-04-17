2b640d82deebf812a76db301cb0dc40cd68cf997
Alban Neumann <aneumann@f2r10s18.clusters.42paris.fr>
2026-04-17 16:03:12 +0200

    EX05

---
+++ ex05/building.py
@@ -0,0 +1,50 @@
+import sys
+
+
+def Count_characters(text: str):
+    num_upper = 0
+    num_tot = 0
+    num_lower = 0
+    num_ponct = 0
+    num_digit = 0
+    num_space = 0
+    for char in text:
+        num_tot += 1
+        if (char.isupper()):
+            num_upper += 1
+        elif (char.islower()):
+            num_lower += 1
+        elif (char.isdigit()):
+            num_digit += 1
+        elif (char.isspace()):
+            num_space += 1
+        else:
+            num_ponct += 1        
+    print(f"The text contains {num_tot} characters:\n{num_upper}"
+          f" upper letters\n{num_lower} lower letters\n{num_ponct}"
+          f" punctuation marks\n{num_space} spaces\n{num_digit} digits")
+    return
+
+
+def main():
+    """Function which count number of characters, digits, upper and lower
+    letters, function marks, and spaces in string. If wrong number of arguments
+    print error message with : AssertionError
+    """
+    if (len(sys.argv) > 2):
+        print("AssertionError: Wrong number of arguments")
+        return
+    elif (len(sys.argv) == 1):
+        print("What is the text to count?")
+        Count_characters(sys.stdin.read())
+    else:
+        Count_characters(sys.argv[1])
+    return
+
+
+if __name__ == '__main__':
+    main()
+
+
+# ressources : https://docs.python.org/fr/3/library/__main__.html
+# ressources : https://www.w3schools.com/python/python_user_input.asp
