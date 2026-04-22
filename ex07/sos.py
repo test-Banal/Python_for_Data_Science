c5d007dbd60097556c80547020b0c0aaab4398f4
aneumann <aneumann@f1r2s2.paris.42.school>
2026-04-22 17:24:21 +0200

    ex07

--- ex07/sos.py
+++ ex07/sos.py
@@ -2,18 +2,18 @@ import sys
 
 
 MORSE_CODE_DICT = {'A': '.-', 'B': '-...',
-                    'C': '-.-.', 'D': '-..', 'E': '.',
-                    'F': '..-.', 'G': '--.', 'H': '....',
-                    'I': '..', 'J': '.---', 'K': '-.-',
-                    'L': '.-..', 'M': '--', 'N': '-.',
-                    'O': '---', 'P': '.--.', 'Q': '--.-',
-                    'R': '.-.', 'S': '...', 'T': '-',
-                    'U': '..-', 'V': '...-', 'W': '.--',
-                    'X': '-..-', 'Y': '-.--', 'Z': '--..',
-                    '1': '.----', '2': '..---', '3': '...--',
-                    '4': '....-', '5': '.....', '6': '-....',
-                    '7': '--...', '8': '---..', '9': '----.',
-                    '0': '-----'}
+                   'C': '-.-.', 'D': '-..', 'E': '.',
+                   'F': '..-.', 'G': '--.', 'H': '....',
+                   'I': '..', 'J': '.---', 'K': '-.-',
+                   'L': '.-..', 'M': '--', 'N': '-.',
+                   'O': '---', 'P': '.--.', 'Q': '--.-',
+                   'R': '.-.', 'S': '...', 'T': '-',
+                   'U': '..-', 'V': '...-', 'W': '.--',
+                   'X': '-..-', 'Y': '-.--', 'Z': '--..',
+                   '1': '.----', '2': '..---', '3': '...--',
+                   '4': '....-', '5': '.....', '6': '-....',
+                   '7': '--...', '8': '---..', '9': '----.',
+                   '0': '-----'}
 
 
 def encrypt(message):
@@ -23,6 +23,7 @@ def encrypt(message):
             result += MORSE_CODE_DICT[letter] + ' '
         else:
             result += '/'
+    return result
 
 
 def main():
@@ -34,8 +35,17 @@ def main():
     """
 #        assert (len(sys.argv == 2)), "wrong number of arguments"
     try:
+        # nbr arg
+        assert len(sys.argv) == 2, \
+            "the arguments are bad"
         message = sys.argv[1]
-        assert isinstance(message, str), "the arguments are bad"
+        # isinstance = check str, > 0 si vide
+        assert len(sys.argv[1]) > 0 and isinstance(message, str), \
+            "the arguments are bad"
+        # caracteres non voulus
+        assert \
+            all(c.upper() in MORSE_CODE_DICT or c == ' ' for c in message), \
+            "the arguments are bad"
         result = encrypt(message.upper())
         print(result)
     except AssertionError as e:
@@ -44,3 +54,31 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+"""   try:
+        if (len(sys.argv) == 2):
+            message = sys.argv[1]
+            assert isinstance(message, str), "the arguments are bad"
+            result = encrypt(message.upper())
+            print(result)
+        else:
+            print("wrong number")
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+
+
+def main():
+    try:
+        assert len(sys.argv) == 2 or len(sys.argv[1] == 0), "Wrong number of 
+        arguments, expected 2 argument"
+        if (len(sys.argv[1]) == 0):
+            print("test nulle")
+        message = sys.argv[1]
+        assert isinstance(message, str), "The arguments are bad"
+        result = encrypt(message.upper())
+        print(result)
+    except AssertionError as e:
+        print(f"AssertionError: {e}")
+    except KeyError as e:
+        print(f"Caractère non supporté : {e}")"""
